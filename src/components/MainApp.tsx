9f6a859e1f9d1f42e8f54b01d9ae532bde4d0a53
Tzofiya Akiva - Rozen <zofiya2070@gmail.com>
2025-01-06 11:02:03 +0200

    change layout

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -12,11 +12,13 @@ const MainApp: React.FC = () => {
   }
 
   return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", padding: "20px" }}>
+    <Navbar />
+    <div style={{ flex: 1 }}>
+       <Outlet /> 
+    </div>
+    <Footer />
+  </div>
   );
 };
 
